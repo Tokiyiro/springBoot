a0f3350dd1dc89a8a5e96197af0d947743161026
Tokiyiro <2780598694@qq.com>
2026-07-23 16:10:06 +0800

    第一次提交
    第一次提交

---
+++ jxc_System/src/main/java/com/lzj/admin/JxcSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lzj.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxcSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxcSystemApplication.class, args);
+    }
+
+}
